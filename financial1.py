80a04abf7e5d9d217b015f0624dd47a51fecdfe6
Stefan Gibitz <stefan@tux1.sghome>
2012-10-15 17:40:05 +0200

    With code.

---
+++ financial1.py
@@ -0,0 +1,19 @@
+# !/bin/python
+
+print "Startkapital in euro eingeben:"
+sKapital = raw_input()
+print "Zinssatz in Prozent (z.B. 5) eingeben:"
+zinssatz = raw_input()
+print "Laufzeit in Jahren eingeben:"
+laufzeit = raw_input()
+
+def zins(sKapital, zinssatz, laufzeit, currentYear):
+    zSum = sKapital 
+    if currentYear <= laufzeit:
+        sKapital += (sKapital/100)*zinssatz
+        currentYear += 1 
+        #print "Jahr: " + str(currentYear) + " " + str(sKapital) + " euro"
+        return zins(sKapital, zinssatz, laufzeit, currentYear)
+    return zSum
+
+print int(zins(float(sKapital), int(zinssatz), int(laufzeit), 1))
